4d2bc977fb5f84a0f761bca05f3750566f5e4754
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-07-30 13:11:25 +0530

    day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,14 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("welcom to java programing ");
+       System.out.println("I already known how to build  programing logic ");
+       System.out.println("NOW i am learning java "); 
+      System.err.println(" I will excited to be a part of kodnest ");
+
+
+
+
+
     }
 }
