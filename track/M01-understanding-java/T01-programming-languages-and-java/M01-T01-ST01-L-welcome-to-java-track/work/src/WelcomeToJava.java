a0b18f313c64eba14e701a81ba5ee2fde69c5357
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-07-30 15:31:38 +0530

    all changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -7,6 +7,6 @@ public class WelcomeToJava {
       System.err.println(" I will excited to be a part of kodnest ");
       System.out.println("today i learn git hub and intellij");
       System.out.println("now i will  push new project ");
-
+System.out.println("i am pragati sakshi and i am very good girl");
     }
 }
