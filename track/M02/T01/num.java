cbfe55ebd5f0ba207fd4ca84e941d69048d0c7ce
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-04 18:13:52 +0530

    num.java

---
+++ track/M02/T01/num.java
@@ -0,0 +1,15 @@
+package track.M02.T01;
+
+public class num {
+    public static void main(String[] args) {
+        int n = 5; // Added declaration for n
+        
+        if (n > 0) {
+            System.out.println("Positive");
+        } else if (n < 0) {
+            System.out.println("Negative");
+        } else { 
+            System.out.println("Zero");
+        } 
+    }
+}
