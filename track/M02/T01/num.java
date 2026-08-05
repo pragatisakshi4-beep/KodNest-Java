42ea5f894857050c1b98a827ac3cd077f1098e4d
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-05 12:53:25 +0530

    changes

--- track/M02/T01/num.java
+++ track/M02/T01/num.java
@@ -1,15 +1,19 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
 package track.M02.T01;
 
 public class num {
-    public static void main(String[] args) {
-        int n = 5; // Added declaration for n
-        
-        if (n > 0) {
-            System.out.println("Positive");
-        } else if (n < 0) {
-            System.out.println("Negative");
-        } else { 
-            System.out.println("Zero");
-        } 
-    }
+   public num() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 5;
+      if (var1 >= 0) {
+         System.out.println("Positive");
+      } else if (var1 < 0) {
+         System.out.println("Negative");
+      } else {
+         System.out.println("Zero");
+      }
+
+   }
 }
---
+++ track/M02/prg2.java
@@ -0,0 +1,11 @@
+package track.M02;
+
+public class prg2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if(i==4);
+            System.out.println("i");
+        }
+             
+    }
+    
