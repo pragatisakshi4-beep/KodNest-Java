e78833b6b98cc9610171ad02693feaaef6f0523a
pragatisakshi <275427708+pragatisakshi4-beep@users.noreply.github.com>
2026-08-07 12:04:49 +0530

    add file

--- track/M01-understanding-java/M02-understanding-java/NestedPrg2.java
+++
@@ -1,7 +0,0 @@
-public class NestedPrg2 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for
-        }
-    }
-}
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/UserInput2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class UserInput2 {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter byte value: ");
+    byte a = scan.nextByte();
+    System.out.println("Byte Value is " + a);
+
+    System.out.println("Enter Short Value: ");
+    short b = scan.nextShort();
+    System.out.println("Short value is " + b);
+
+    System.out.println("Enter Integer Value: ");
+    int c = scan.nextInt();
+    System.out.println("Integer value is " + c);
+
+    System.out.println("Enter Long value: ");
+    long d = scan.nextLong();
+    System.out.println("Long value is " + d);
+
+    System.out.println("Enter Float value: ");
+    float e = scan.nextFloat();
+    System.out.println("Float value is " + e);
+
+    System.out.println("Enter double value: ");
+    double f = scan.nextDouble();
+    System.out.println("Double value is " + f);
+
+    System.out.println("Enter Character value: ");
+    char g = scan.nextChar();
+    System.out.println("Character value is " + g);
+
+    System.out.println("Enter boolean value: ");
+    boolean h = scan.nextBoolean();
+    System.out.println("Boolean value is " + h);
+  }
+}
